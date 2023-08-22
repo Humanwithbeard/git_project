73bc18ae6f6c91627e1606159ba70228ce62b401
Denis Ukrajev <142827776+Humanwithbeard@users.noreply.github.com>
2023-08-22 20:37:14 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 print("Hello linux")
-
-=======
-print("Hello World")
->>>>>>> 47ab4ef (edit print('Hello world'))
-=======
-print("hello world")
->>>>>>> 5531878 (print(hello world))
